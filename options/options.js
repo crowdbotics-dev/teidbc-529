f7e91e1e0d2a7053c2b75e72cc3072cc7fc9591d
Crowdbotics <engineering@crowdbotics.com>
2024-07-23 10:30:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "teidbc-529",
+  url: "https://teidbc-529.botics.co",
+  api: "https://teidbc-529.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
